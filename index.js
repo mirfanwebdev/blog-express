3540055ebdd1b6f3b27763b1d058af8e09797669
m.irfan <m.irfanwebdev@gmail.com>
2024-12-23 22:48:58 +0700

    initialize express, cors, supabase-js

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+// config
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// start server
+app.listen(PORT, () => {
+  console.log("Server running on port 3000");
+});
